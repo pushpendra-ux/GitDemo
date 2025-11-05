0a0ec4d75dc161d08c0d102bf17c053b74720497
pushpendra-ux <pmund2707@gmail.com>
2025-11-05 18:22:03 +0530

    initial Commit

---
+++ Task1.py
@@ -0,0 +1,4 @@
+# git config --global user.name "pushpendra-ux"
+# git config --global user.email "pmund2707@gmail.com"
+# git config --global color.ui auto
+# git remote add origin https://github.com/pushpendra-ux/GitDemo.git
